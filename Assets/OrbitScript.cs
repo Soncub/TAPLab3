110bd3e4ddafb987244a74d0e6c79cc5d8601664
Andrew Mokhtary <149095320+Soncub@users.noreply.github.com>
2024-09-15 16:05:07 -0400

    Check out

--- Assets/OrbitScript.cs
+++ Assets/OrbitScript.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class OrbitScript : MonoBehaviour
 {
     [SerializeField] private float orbitSpeed = 20f;
-    [SerializeField] private Transform orbitTarget;
+    [SerializeField] private GameObject orbitTarget;
     private Vector3 axis;
+    private float targetStartDistance;
     [SerializeField] private bool reverse = false;
 
     void Start()
     {
+        targetStartDistance = Vector2.Distance(orbitTarget.transform.position, transform.position);
+
         if (reverse == true)
         {
             orbitSpeed = -1 * orbitSpeed;
@@ -19,7 +22,15 @@ public class OrbitScript : MonoBehaviour
 
     void Update()
     {
-        axis = orbitTarget.position;
-        transform.RotateAround(axis, Vector3.forward, orbitSpeed * Time.deltaTime);
+        //get ratio of initial player distance to current distance
+        float targetDistance = Vector2.Distance(orbitTarget.transform.position, transform.position);
+        float speedScale = Mathf.InverseLerp(targetStartDistance, 0f, targetDistance);
+
+        axis = orbitTarget.transform.position;
+        transform.RotateAround(axis, Vector3.forward, (speedScale * orbitSpeed * Time.deltaTime));
+        //                                                 ^ratio is now the coefficient of the orbit speed
+
+        float speed = speedScale * orbitSpeed;
+        Debug.Log("speedScale:" + speedScale);
     }
 }
